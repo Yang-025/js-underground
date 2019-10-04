1aaf5878c6ab4591d687646c0a9572ff98633975
Yang025 <kkadso@gmail.com>
2019-10-04 10:10:56 +0800

    99乘法表的卡片

--- js-underground-app/src/APP1F/MultiplicationChart.tsx
+++ js-underground-app/src/APP1F/MultiplicationChart.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './MultiplicationChart.scss';
+import MultiplicationCard from './MultiplicationCard';
 
 
 const MultiplicationChart: React.FC = () => {
+    const cardsArray = Array.from(Array(9).keys()).filter(x => x > 0).map(cardIndex => {
+        return <MultiplicationCard number={cardIndex + 1} />;
+    });
     return (
-        <div className="multiplication-chart-container"> 
-            <div className="wrapper">MultiplicationChart</div>
-            <footer><p>Copyright © 2019 HexSchool. All rights reserved.</p></footer>
+        <div className="multiplication-chart-container">
+            <div className="multiplication-chart-wrapper">
+                title
+                {cardsArray}
+            </div>
+
+            {/* <footer><p>Copyright © 2019 HexSchool. All rights reserved.</p></footer> */}
         </div>
 
     );
