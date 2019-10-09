b25ba46739a5cc8eff29995e2f0dc4c022222a04
Yang025 <kkadso@gmail.com>
2019-10-09 16:43:59 +0800

    更新thanos筆記

--- js-underground-app/src/APP15F/utils.tsx
+++ js-underground-app/src/APP15F/utils.tsx
@@ -66,24 +66,52 @@ export function initLayersList(layerCount: number, width: number, height: number
 
 
 /*
-sourceImgData: 原本的圖片
+sourceImgData: 原本的圖片ImageData
 */
-export function sampler(imgDatas: ImageData[], sourceImgData: ImageData, width: number, height: number, layerCount: number) {
-  let newImgDatas = R.clone(imgDatas);
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
+export function sampler(sourceImgData: ImageData, canvasWidth: number, canvasHeight: number, layerCount: number) {
+  // 創造n個空白的ImageData Instance陣列
+  const emptyLayersList: Array<ImageData> = initLayersList(layerCount, canvasWidth, canvasHeight);
+  
+  // 把抽到的index補上像素
+  let newImgDatas = R.clone(emptyLayersList);
+  for (let x = 0; x < canvasWidth; x++) {
+    for (let y = 0; y < canvasHeight; y++) {
       for (let l = 0; l < 2; l++) {
-        // x方向的random
-        // const pieceIndex = Math.floor(layerCount * (Math.random() + 2 * x / width) / 3);
-        // y方向的random
-        const pieceIndex = Math.floor(layerCount * (Math.random() + 2 * y / height) / 3);
-        const pixelPos = 4 * (y * width + x);
+        // 把原本的圖片分散到n層
+        // x方向的分散到n層
+        const layerIndex = Math.floor(layerCount * (Math.random() + 2 * x / canvasWidth) / 3);
+        // y方向的分散到n層
+        // const layerIndex = Math.floor(layerCount * (Math.random() + 2 * y / canvasHeight) / 3);
+        
+        // 取得像素R的位子
+        const pixelPos = 4 * (x + y * canvasWidth);
+        // 找到R就找到R,G,B,A
         for (let rgbaIndex = 0; rgbaIndex < 4; rgbaIndex++) {
           const dataPos = pixelPos + rgbaIndex;
-          newImgDatas[pieceIndex].data[dataPos] = sourceImgData.data[dataPos];
+          newImgDatas[layerIndex].data[dataPos] = sourceImgData.data[dataPos];
         }
       }
     }
   }
   return newImgDatas;
+}
+
+
+/*
+把canvas層補到畫面上
+*/
+export function appendLayersWithParticleEffect(samplerLayersList: ImageData[], canvasWidth: number, canvasHeight: number, el: HTMLElement) {
+  const updatedCanvasList = samplerLayersList.map((layerImageData) => {
+    const layerCanvasEl = document.createElement('canvas');
+    const chuckCtx = layerCanvasEl.getContext('2d')!;
+    layerCanvasEl.width = canvasWidth;
+    layerCanvasEl.height = canvasWidth;
+    // 動畫效果
+    layerCanvasEl.style.transition = `transform 1.2s ease-out, opacity 1.5s ease-out`;
+    chuckCtx.putImageData(layerImageData, 0, 0);
+    // 把這個<canvas>放到指定的<div>裡面
+    el.appendChild(layerCanvasEl);
+    return layerCanvasEl;
+  });
+  return updatedCanvasList;
 }
